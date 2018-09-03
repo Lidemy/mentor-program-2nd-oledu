368a138a731dfc61f55a5adc69af142cae5126be
Xu Jinbin <petrer801014@gmail.com>
2018-09-04 07:11:00 +0800

    hw3 complete

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,17 @@
 function reverse(str) {
-
+	var str_rev ='';
+	for(var i=str.length-1;i>=0;i--){
+		str_rev += str[i];
+	}
+	console.log(str_rev);
 }
+
+
+
+/*
+reverse('abcabc');
+
+reverse('how old are you?')
+
+reverse('')
+*/
