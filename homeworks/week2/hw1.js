79c808414070f0e631e4ebe592895d2919fdf437
Xu Jinbin <petrer801014@gmail.com>
2018-09-07 07:55:13 +0800

    hw1,2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,4 +1,13 @@
 function stars(n) {
+	var star = [];
+	for (var i=1; i<=n;i++) {
+		var inner_star = '';
+		for(var i=1;i<=n;i++){
+			inner_star+='*';
+			star.push(inner_star);
+		}
+	}
+	console.log(star);
 
 }
 
