c9af5a92caad37a54cd794c17d60de1838084f14
Xu Jinbin <petrer801014@gmail.com>
2018-09-10 23:27:41 +0800

    new

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -6,12 +6,18 @@ function add(a, b) {
 	var lArray = longerNum.split('').reverse();
 	var sArray = shorterNum.split('').reverse()
 
+	console.log(lArray);
+	console.log(sArray);
+
 	var diff = longerNum.length - shorterNum.length;
-	while(diff>1){
+	while(diff>0){
 		sArray.push('0');
 		diff--;
 	}
-	
+
+	console.log(lArray);
+	console.log(sArray);
+
 	var sumArray = [];
 	for (var i = 0; i < lArray.length; i++) {
 		var digitSum = parseInt(lArray[i]) + parseInt(sArray[i]);
@@ -24,5 +30,5 @@ function add(a, b) {
 	}
 	return(sumArray.reverse().join(''));
 }
- 
+console.log(add('1','999'))
 module.exports = add;
