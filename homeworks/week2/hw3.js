16da82c17f16430c04eea9afbc6a7c48e393393b
Xu Jinbin <petrer801014@gmail.com>
2018-09-10 23:39:28 +0800

    new

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -11,11 +11,9 @@ function isPrime(n) {
 			for(var i=2;i<n;i++){
 				if(n%i==0){
 					return false;
-					break;
 					console.log(44)
 				}else if(i==n-1){
 					return true;
-					break;
 				}
 			}
 	}
