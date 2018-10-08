e968da85642aa248199dda644991b758046aebba
Xu Jinbin <xujinbin@Xude-MacBook-Pro.local>
2018-10-08 08:22:27 +0800

    hw2

--- homeworks/week4/hw2/script.js
+++ homeworks/week4/hw2/script.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   addEventListener('click', (e) => {
     let submit = document.querySelector('.submit');
+    console.log(e.target.name);
+
     if (e.target.name === 'submit') {
-      document.querySelectorAll('.items').style.cssText = "background-color:red";
+      //let red = document.querySelectorAll('.items');
       let items = document.querySelectorAll('.required > input')
-
-      let logArray = []
+      console.log(items[0].type);
       for (let i = 0; i < items.length; i++) {
-        logArray.push(0);
-      }
-      for (let i = 0; i < logArray.length; i++) {
-        if (false) {
-          console.log();
+        if (items != 'radio' && items[i].value === '') {
+          items[i].parentElement.style['background-color'] = 'rgb(243, 189, 189)';
+          items[i].style['background-color'] = 'rgb(243, 189, 189)';
+        } else {
+          items[i].parentElement.style['background-color'] = 'white';
+          items[i].style['background-color'] = 'white';
         }
-
       }
-    }
+      let radioValue = document.querySelector('input[name="class"]:checked')
+      if (radioValue === null) {
+        document.querySelector('input[name="class"]').parentElement.style['background-color'] = 'rgb(243, 189, 189)';
+      }
 
+    }
   })
 })
-
-  background - color: rgb(243, 219, 224)
-
--- homeworks/week4/hw5.md
+++ homeworks/week4/hw5.md
@@ -1,24 +1,30 @@
 1. 什麼是 DOM？
-   DOM 建立起網頁與程式語言互動的橋樑，把 HTML 文件結構化，並讓 Script 文件可以存取及改變網頁的內容和風格。
+  DOM 建立起網頁與程式語言互動的橋樑，把 HTML 文件結構化，並讓 Script 文件可以存取及改變網頁的內容和風格。
 
 2. 什麼是 Ajax？
-   Ajax (Asynchronous 非同步 JavaScript and XML) 是一種讓網頁不必重新讀取就可以送出 request 的技術。
+  Ajax (Asynchronous 非同步 JavaScript and XML) 是一種讓網頁不必重新讀取就可以送出 request 的技術。
 
 3. HTTP method 有哪幾個？有什麼不一樣？
 
+
 4. `GET` 跟 `POST` 有哪些區別，可以試著舉幾個例子嗎？
+  GET會把要傳送的資料包含在網 址列裡面，例如 google 搜尋時網址列會出現 "search?q=....."
+  POST則是會把要傳送的資料包含在 request 裡面。
+
 5. 什麼是 RESTful API？
+  RESTFUL 是 Representational State Transfer 的縮寫，是一種網站設計的標準，沒有硬性規定。
+  REST API 充分的使用了 HTTP 的 Method 達到直觀簡潔的 URI 、善用 HTTP Verb達到對資源的操作。
 
 6. JSON 是什麼？
-   JSON 是資料儲存的格式，方便處理也容易閱讀。格式類似以下：
-   {
-   "firstName": "Peter",
-   "lastName": "Xu",
-   "sex": "male",
-   "age": 25,
-   }
+  JSON 是資料儲存的格式，方便處理也容易閱讀。格式類似以下：
+  {
+  "firstName": "Peter",
+  "lastName": "Xu",
+  "sex": "male",
+  "age": 25,
+  }
 
 7. JSONP 是什麼？
-   JASONP 是一種利用 <script> 標籤的特性，讓網頁可以繞過 Same Origin Policy 來得到 response 的資料。
+  JASONP 是一種利用 <script> 標籤的特性，讓網頁可以繞過 Same Origin Policy 來得到 response 的資料。
 
 8. 要如何存取跨網域的 API？
