8cc9e4705cff5f3c45171b954ba0a19ce88e39cc
Xu Jinbin <xujinbin@Xu.local>
2018-10-13 21:04:01 +0800

    g

--- homeworks/week5/hw2/conn.php
+++ homeworks/week5/hw2/conn.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mentor";
+$servername = "166.62.28.131";
+$username = "student2nd";
+$password = "mentorstudent123";
+$dbname = "mentor_program_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    echo "fail";
 }
 ?>
