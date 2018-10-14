ba31e771543ab7c6d2baa78d29d112b9d65d9e72
Xu Jinbin <xujinbin@Xu.local>
2018-10-15 00:16:07 +0800

    week5

--- homeworks/week5/hw3/conn.php
+++ homeworks/week5/hw3/conn.php
@@ -6,6 +6,7 @@ $dbname = "mentor_program_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 //$conn->query("SET NAMES 'UTF8'"); 
+$conn->query('SET time_zone = "+8:00"');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- homeworks/week5/hw3/logout.php
+++ homeworks/week5/hw3/logout.php
@@ -1,9 +1,9 @@
 <?php session_start(); ?>
 <?php
   unset($_COOKIE);
-  echo '登出中\n導入登入頁面';
+  echo '登出中<br>導入登入頁面';
   setcookie('username', '', time() - 3600); 
-  header("refresh:0;url=./login.php");     
+  header("refresh:3;url=./login.php");     
 
     
 ?>
